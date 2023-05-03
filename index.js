81666853acfa443d810fd373d5bdd8f86cb98a1a
Patryk <sucheckipatryk@gmail.com>
2023-05-03 15:58:35 -0400

    Scope Lab Complete

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Write your solution in this file!
+//Test 1
+var customerName = 'bob';
+
+//Test 2
+function returnCustomerName(customerName)  {
+    return;
+}
+
+//Test 3
+function upperCaseCustomerName() {
+    customerName = customerName.toUpperCase();   
+}
+
+//Test 4
+function setBestCustomer() {
+    bestCustomer = "not bob";
+}
+
+//Test 5
+function overwriteBestCustomer() {
+    bestCustomer = "maybe bob";
+}
+
+//Test 6
+const leastFavoriteCustomer = "";
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = 'nope';
+}
